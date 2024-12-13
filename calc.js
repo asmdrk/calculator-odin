cb29c2d0fd58fdc6d9d0fee8adfb2a55dc19ba47
asmdrk <goodboydiwaker@gmail.com>
2024-12-13 13:19:43 +0530

    added html and stylesheet and basic layout

---
+++ calc.js
@@ -0,0 +1,27 @@
+function add(a,b) {
+    return a + b;
+}
+function sub(a,b) {
+    return a - b;
+}
+
+function mul(a,b) {
+    return a*b;
+}
+
+function div(a,b) {
+    return a/b;
+}
+
+function operator(a,b,op) {
+    switch (op) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return sub(a,b);
+        case "*":
+            return mul(a,b);
+        case "/":
+            return div(a,b);
+    }
+}
